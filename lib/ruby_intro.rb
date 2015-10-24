c980c4dc8b534bf96bf33a5eb84bc5ed18aae1c2
lispincsclothing <scullingnull@gmail.com>
2015-10-24 05:33:07 +0000

    Part 1 and Part of 2 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,31 +3,49 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.empty? ? 0 : arr.inject(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.empty? ? 0 : arr.sort.last(2).inject(:+)
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  # return false if arr.empty? - taken care of by spaceship operator et al 
+  left = 0
+  right = arr.length - 1
+  arr.sort!
+  while left < right
+    case arr[left] + arr[right] <=> n
+    when 0
+      return true
+    when 1
+      right -= 1
+    when -1
+      left += 1
+    end
+  end
+  false 
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, "+name
+  
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s[0] =~ /[^aeiou\W]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
 
+  # s =~ /^[01]*1[01]*00$|[0]$/ 
+  # above does not work for case 0 
+  s =~ /^([048]|[0-9]*([02468][048]|[13579][26]))$/i #why work? 
+end
+  
 # Part 3
 
 class BookInStock
